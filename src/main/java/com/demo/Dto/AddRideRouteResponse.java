1925c3e646e786f3e33c158eca6b1cc48edaa7d0
Varadraj Jagdale <144721104+Varadrajj@users.noreply.github.com>
2024-02-21 01:56:18 +0530

    Delete src/main directory

--- src/main/java/com/demo/Dto/AddRideRouteResponse.java
+++
@@ -1,9 +0,0 @@
-package com.demo.Dto;
-
-public class AddRideRouteResponse extends BaseResponse {
-
-	public AddRideRouteResponse(boolean status, String message) {
-		super(status, message);
-	}
-	
-}
